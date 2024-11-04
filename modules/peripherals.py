95ad8fceebbcd77c275b0333ee7ece771d18c5e6
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2024-11-03 21:29:07 -0500

    Update peripherals.py

--- modules/peripherals.py
+++ modules/peripherals.py
@@ -1,6 +1,8 @@
 from gpiozero import LED, Buzzer
 import time
 
+from modules import report
+
 #LEDs
 red_led = LED(1) # TODO: Figure out which pin to put the red LED into
 green_led = LED(2) # TODO: Figure out which pin to put the green LED into
@@ -8,8 +10,16 @@ white_led = LED(3) # TODO: Figure out which pin to put the white LED into
 
 def red_LED():
     red_led.toggle()
+    if red_led.value == 0:
+        report.report_update("Red LED is off.")
+    else:
+        report.report_update("red LED is on.")
 def white_LED():
     white_led.toggle()
+    if white_led.value == 0:
+        report.report_update("White LED is off.")
+    else:
+        report.report_update("White LED is on.")
 
 def green_LED(): #NOT FOR COMMAND USE, ONLY FOR SYSTEM USE
     green_led.toggle()
@@ -18,12 +28,15 @@ def red_LED_on(): #FOR SYSTEM USE
     time.sleep(3.5)
     red_led.off()
 
-
 #Buzzer
 buzzer = Buzzer(3) # TODO 3: Figure out which pin to put the Piezo butter into
 
 def buzz():
     buzzer.toggle()
+    if buzzer.value == 0:
+        report.report_update("Buzzer is off.")
+    else:
+        report.report_update("Buzzer is on.")
 
 def beep(interval):
-    buzzer.beep(interval, interval)
+        buzzer.beep(interval, interval, 8)
