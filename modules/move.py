c000f9b45bb9e8c844759aa1abbff1376a2ac761
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2024-11-04 20:42:49 -0500

    Completely overhauled move module
    originally it was going to work with the built-in "robot" functionality of gpiozero module, but I top-downed a script that will work with an L298N motor driver and an external 9V battery (might update to 12V if I can find a rechargeable one

--- modules/move.py
+++ modules/move.py
@@ -1,38 +1,55 @@
-import gpiozero as gpio
+from gpiozero import Motor
+from gpiozero.pins.pigpio import PiGPIOFactory
 import time
 
+from modules import report
 from modules import peripherals as prp
 
-robot = gpio.Robot(left=gpio.Motor(1, 2), right=gpio.Motor(3, 4)) # TODO: Figure out which pin to put the motors into
+# Use PiGPIOFactory to avoid conflict with RPi.GPIO if needed
+factory = PiGPIOFactory()
 
-def move_forward(cm): #ESTIMATED RATE IS 1CM/SECOND
-    prp.green_LED()
-    robot.forward()
+# Configure motor pins
+motor_left = Motor(forward="BOARD16", backward="BOARD18", pin_factory=factory)  # IN1, IN2 for left motor
+motor_right = Motor(forward="BOARD24", backward="BOARD26", pin_factory=factory)   # IN3, IN4 for right motor
+
+def move_forward(cm): #estimated speed is 1cm/second
+    motor_left.forward()
+    motor_right.forward()
+    report.report_update(f"Moving forward {cm}cm")
     time.sleep(cm)
-    prp.green_LED()
-    robot.stop()
+    motor_left.stop()
+    motor_right.stop()
+    report.report_update("Stopped")
 
 def move_backward(cm):
-    prp.green_LED()
-    robot.backward()
+    motor_left.backward()
+    motor_right.backward()
+    report.report_update(f"Moving backward {cm}cm")
     time.sleep(cm)
-    prp.green_LED()
-    robot.stop()
+    motor_left.stop()
+    motor_right.stop()
+    report.report_update("Stopped")
 
-def move_left(cm):
-    prp.green_LED()
-    robot.left()
-    time.sleep(cm)
-    prp.green_LED()
-    robot.stop()
+def turn_left(degrees): #estimated turn-rate is 1º/second
+    motor_left.backward()
+    motor_right.forward()
+    report.report_update(f"Turning left {degrees}º")
+    time.sleep(degrees)
+    motor_left.stop()
+    motor_right.stop()
+    report.report_update("Stopped")
 
-def move_right(cm):
-    prp.green_LED()
-    robot.right()
-    time.sleep(cm)
-    prp.green_LED()
-    robot.stop()
+def turn_right(degrees):
+    motor_left.forward()
+    motor_right.backward()
+    report.report_update(f"Turning right {degrees}º")
+    time.sleep(degrees)
+    motor_left.stop()
+    motor_right.stop()
+    report.report_update("Stopped")
 
 def stop():
+    motor_left.stop()
+    motor_right.stop()
+    report.report_update("Stopped")
     prp.red_LED_on()
-    robot.stop()
