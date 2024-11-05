a190f610d90ce9556a3051291dde0e8a2f03b44b
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2024-11-05 12:30:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
-import customtkinter as ctk
+import requests
+import time
 
-ctk.set_default_color_theme("themes/violet.json")
+command_list_help = {
+    "fwd:(centimeters)": "Robot moves forward the amount of centimeters inputted",
+    "bkd:(centimeters)": "Robot moves backward the amount of centimeters inputted",
+    "left:(degrees)": "Robot turns left the amount of degrees inputted",
+    "right:(degrees)": "Robot turns right the amount of degrees inputted",
+    "red_LED": "Built in Red LED turns on",
+    "white_LED": "Built in White LED turns on",
+    "buzzer": "Built in Piezo buzzer starts buzzing",
+    "buzzer_beep:(seconds)": "Built in Piezo buzzer beeps 8 times with an interval of seconds inputted"
+}
+
+command_list_valid = [
+    "fwd:", "bkd:", "left:", "right:", "red_LED", "white_LED", "buzzer", "buzzer_beep:"
+]
+
+def main():
+    print("Type 'HELP' to list commands")
+    print(command_list_help.keys())
+    while True:
+        get_command()
+
+def get_command():
+        command = str(input(": "))
+        if command.upper() == 'HELP':
+            for command in command_list_help:
+                print(command + " -> " + command_list_help[command] + "\n")
+        elif command in command_list_valid:
+            requests.get("http://localhost:5000/give_command", params={"command":command})
+            time.sleep(0.15)
+            print(requests.get("http://localhost:5000/see_app_update").content.decode())
+            time.sleep(0.15)
+            print(requests.get("http://localhost:5000/see_app_update").content.decode())
+        else:
+            print("Invalid command")
+
+if __name__ == "__main__":
+    main()
