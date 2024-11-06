98a35499d76681d4232c6e2c161660602a7d65bb
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2024-11-06 09:27:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import requests
 import time
-import subprocess
 
 command_list_help = {
     "fwd:(centimeters)": "Robot moves forward the amount of centimeters inputted",
@@ -18,31 +17,53 @@ command_list_valid = [
 ]
 
 def main():
-    print("'Help' for help, 'Errors' for errors, 'Updates' for full updates")
     while True:
         get_command()
 
 
 def get_command():
-        command = str(input(": "))
+        command = str(input(">> "))
         #system command checks
         if command.upper() == 'HELP':
             for command in command_list_help:
                 print(command + " -> " + command_list_help[command] + "\n")
         elif command.upper() == 'ERRORS':
-            print(requests.get("http://localhost:5000/see_errors").content.decode('utf-8'))
+            try:
+                print(requests.get("http://localhost:5000/see_errors").content.decode('utf-8'))
+            except Exception as e:
+                print(e)
+                print("\n" * 5)
+                main()
         elif command.upper() == 'UPDATES':
-            print(requests.get("http://localhost:5000/see_updates").content.decode('utf-8'))
+            try:
+                print(requests.get("http://localhost:5000/see_updates").content.decode('utf-8'))
+            except Exception as e:
+                print(e)
+                print("\n" * 5)
+                main()
+        elif command.upper() == 'CLEAR':
+            try:
+                print("\n" * 50)
+            except Exception as e:
+                print(e)
+                print("\n" * 5)
+                main()
 
         #robot command check
         elif command[0:4] in command_list_valid:
-            requests.get("http://localhost:5000/give_command", params={"command":command})
-            time.sleep(0.15)
-            print(requests.get("http://localhost:5000/see_app_update").content.decode('utf-8'))
-            time.sleep(0.15)
-            print(requests.get("http://localhost:5000/see_app_update").content.decode('utf-8'))
+            try:
+                requests.get("http://localhost:5000/give_command", params={"command":command})
+                time.sleep(0.15)
+                print("    " + requests.get("http://localhost:5000/see_app_update").content.decode('utf-8') + "\n")
+                time.sleep(0.15)
+                print("    " + requests.get("http://localhost:5000/see_app_update").content.decode('utf-8') + "\n")
+            except Exception as e:
+                print(e)
+                print("\n" * 5)
+                main()
         else:
             print("Invalid command")
 
 if __name__ == "__main__":
+    print("'Help' for help, 'Errors' for errors, 'Updates' for full updates, 'Clear' to clear screen")
     main()
