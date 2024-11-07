fd295c6c086b0627362390152614c23bd602f901
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2024-11-07 18:13:04 -0500

    Update move.py

--- modules/move.py
+++ modules/move.py
@@ -1,4 +1,4 @@
-from gpiozero import Motor
+from gpiozero import Motor, PWMOutputDevice
 from gpiozero.pins.pigpio import PiGPIOFactory
 import time
 
@@ -9,46 +9,66 @@ from modules import peripherals as prp
 factory = PiGPIOFactory()
 
 # Configure motor pins
-motor_left = Motor(forward="BOARD16", backward="BOARD18", pin_factory=factory)  # IN1, IN2 for left motor
-motor_right = Motor(forward="BOARD24", backward="BOARD26", pin_factory=factory)   # IN3, IN4 for right motor
+ena = PWMOutputDevice("BOARD36", pin_factory=factory)
+enb = PWMOutputDevice("BOARD38", pin_factory=factory)
+motor_left = Motor(forward="BOARD16", backward="BOARD18", pin_factory=factory)
+motor_right = Motor(forward="BOARD24", backward="BOARD26", pin_factory=factory)
 
-def move_forward(cm): #estimated speed is 1cm/second
+def move_forward(cm): #estimated speed is 10cm/second
+    ena.value = 1.0
+    enb.value = 1.0
     motor_left.forward()
     motor_right.forward()
     report.report_update(f"Moving forward {cm}cm")
-    time.sleep(cm)
+    time.sleep(cm/10)
     motor_left.stop()
     motor_right.stop()
+    ena.value = 0.0
+    enb.value = 0.0
     report.report_update("Stopped")
 
 def move_backward(cm):
+    ena.value = 0.8
+    enb.value = 0.8
     motor_left.backward()
     motor_right.backward()
     report.report_update(f"Moving backward {cm}cm")
-    time.sleep(cm)
+    time.sleep(cm/10)
     motor_left.stop()
     motor_right.stop()
+    ena.value = 0.0
+    enb.value = 0.0
     report.report_update("Stopped")
 
-def turn_left(degrees): #estimated turn-rate is 1º/second
+def turn_left(degrees): #estimated turn-rate is 10º/second
+    ena.value = 1.0
+    enb.value = 1.0
     motor_left.backward()
     motor_right.forward()
     report.report_update(f"Turning left {degrees}º")
-    time.sleep(degrees)
+    time.sleep(degrees/10)
     motor_left.stop()
     motor_right.stop()
+    ena.value = 0.0
+    enb.value = 0.0
     report.report_update("Stopped")
 
 def turn_right(degrees):
+    ena.value = 1.0
+    enb.value = 1.0
     motor_left.forward()
     motor_right.backward()
     report.report_update(f"Turning right {degrees}º")
-    time.sleep(degrees)
+    time.sleep(degrees/10)
     motor_left.stop()
     motor_right.stop()
+    ena.value = 0.0
+    enb.value = 0.0
     report.report_update("Stopped")
 
 def stop():
+    ena.value = 0.0
+    enb.value = 0.0
     motor_left.stop()
     motor_right.stop()
     report.report_update("Stopped")
