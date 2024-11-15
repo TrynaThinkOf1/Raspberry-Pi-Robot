93133eb4b9d709f69a3936492e2bcb96a81e9b6d
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-15 12:21:39 -0500

    using RpiL library

--- robot.py
+++ robot.py
@@ -1,86 +1,21 @@
-import RPi.GPIO as GPIO
-from time import sleep
+from RpiL import Motor_Driver
 
-command_list_valid = {"w", "a", "s", "d", "c"}
+driver = Motor_Driver(17, 27, 4, 5, 6, 13)
 
-GPIO.setwarnings(False)
+while True:
+    cmd = input(">> ")
 
-# Right Motor
-in1 = 17
-in2 = 27
-en_a = 4
-# Left Motor
-in3 = 5
-in4 = 6
-en_b = 13
+    if cmd == 'w':
+        driver.forward()
 
+    if cmd == 's':
+        driver.backward()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en_a,GPIO.OUT)
+    if cmd == 'a':
+        driver.turn_left()
 
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en_b,GPIO.OUT)
+    if cmd == 'd':
+        driver.turn_right()
 
-q=GPIO.PWM(en_a,100)
-p=GPIO.PWM(en_b,100)
-p.start(75)
-q.start(75)
-
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-GPIO.output(in3,GPIO.LOW)
-
-try:
-    while(True):
-        command = input(">> ")
-        
-        if command in command_list_valid:
-            if command == 'w':
-                GPIO.output(in1,GPIO.HIGH)
-                GPIO.output(in2,GPIO.LOW)
-                
-                GPIO.output(in4,GPIO.HIGH)
-                GPIO.output(in3,GPIO.LOW)
-                print("Forward")
-                
-            elif command == 's':
-                GPIO.output(in1,GPIO.LOW)
-                GPIO.output(in2,GPIO.HIGH)
-                
-                GPIO.output(in4,GPIO.LOW)
-                GPIO.output(in3,GPIO.HIGH)
-                print("Backward")
-                
-            elif command == 'd':
-                GPIO.output(in1,GPIO.LOW)
-                GPIO.output(in2,GPIO.HIGH)
-                
-                GPIO.output(in4,GPIO.HIGH)
-                GPIO.output(in3,GPIO.LOW)
-                print("Right")
-                
-            elif command == 'a':
-                GPIO.output(in1,GPIO.HIGH)
-                GPIO.output(in2,GPIO.LOW)
-                
-                GPIO.output(in4,GPIO.LOW)
-                GPIO.output(in3,GPIO.HIGH)
-                print("Left")
-            
-            elif command == 'c':
-                GPIO.output(in1,GPIO.LOW)
-                GPIO.output(in2,GPIO.LOW)
-                
-                GPIO.output(in4,GPIO.LOW)
-                GPIO.output(in3,GPIO.LOW)
-                print("Stop")
-        else:
-            print("Invalid Command \n")
-
-except KeyboardInterrupt:
-  GPIO.cleanup()
-  print("GPIO Clean up")
+    if cmd == 'c':
+        driver.stop()
